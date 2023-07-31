7c748c8e4087c9b88eea53f911ad61d9dca58588
EmmaBroudic <emma.broudic35@gmail.com>
2023-07-31 15:39:51 +0200

    avancées responsive slider

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route /*, Navigate, useParams*/ } from 'react-router-dom';
 import './index.css';
 import Home from './pages/home';
@@ -8,21 +8,24 @@ import Header from './components/Header/header';
 import Footer from './components/Footer/footer';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Router>
-      <Header />
-      <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/contact" element={<Contact />} />
-      </Routes>
-      <Footer />
-    </Router>
-  </React.StrictMode>
-);
+const container = document.getElementById('root');
+const root = createRoot(container);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+function App() {
+
+  return (
+    <React.StrictMode>
+      <Router>
+        <Header />
+        <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/contact" element={<Contact />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </React.StrictMode>
+  );
+}
+
+root.render(<App />);
 reportWebVitals();
